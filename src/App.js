4c7b87257b49028ec4fb863ac582a8fbbc6c42da
NullDefault <davisha999@gmail.com>
2021-01-22 15:52:57 -0500

    react rewrite

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import {
+  ChakraProvider,
+  Box,
+  Text,
+  Link,
+  VStack,
+  Code,
+  Grid,
+  theme,
+} from '@chakra-ui/react';
+import { ColorModeSwitcher } from './components/ColorModeSwitcher';
+import { Logo } from './components/Logo';
+
+function App() {
+  return (
+    <ChakraProvider theme={theme}>
+      <Box textAlign="center" fontSize="xl">
+        <Grid minH="100vh" p={3}>
+          <ColorModeSwitcher justifySelf="flex-end" />
+          <VStack spacing={8}>
+            <Logo h="40vmin" pointerEvents="none" />
+            <Text>
+              TEDxDrewUniversity website is currently in development.
+            </Text>
+            <Link
+              color="blue.800"
+              href="https://www.drew.edu/"
+              fontSize="2xl"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Drew University
+            </Link>
+          </VStack>
+        </Grid>
+      </Box>
+    </ChakraProvider>
+  );
+}
+
+export default App;
