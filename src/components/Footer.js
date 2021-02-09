4662b4691c2275d8ef1149f26286dc398e61c2a3
NullDefault <davisha999@gmail.com>
2021-02-08 20:29:07 -0500

    Footer fixes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -23,21 +23,7 @@ import { tedxRed } from '../utils/tedxColors';
 export const Footer = (props) => <Flex as="footer" py="8rem" bg="black" color="white" {...props}>
   <VStack spacing="24px">
     <Image src={tedxTextLogo} w="60vmin" />
-    <Stack direction={['column', 'row']} align="center">
-      <VStack w={'50%'}>
-        <Text pl="8%" fontSize="lg">
-          <Text fontSize="3xl" color={tedxRed}>
-            What is TEDx?</Text>
-          <br />
-          In the spirit of ideas worth spreading, TED has created a program called TEDx. TEDx is a program
-          of local, self-organized events that bring people together to share a TED-like experience. Our event is
-          called TEDxDrewUniversity, where x = independently organized TED event. At our TEDxDrewUniversity event,
-          TEDTalks video
-          and live speakers will combine to spark deep discussion and connection in a small group. The TED Conference
-          provides general guidance for the TEDx program, but individual TEDx events, including ours, are
-          self-organized.
-        </Text>
-      </VStack>
+    <Stack direction={['column', 'row-reverse']} justifyContent={['end', 'start']} align="center">
       <VStack w={'50%'} align={['center', 'left']} py="10%">
         <Text fontSize="3xl" color={tedxRed}>Navigation</Text>
 
@@ -61,11 +47,25 @@ export const Footer = (props) => <Flex as="footer" py="8rem" bg="black" color="w
         <NavLink href="https://www.facebook.com/TEDxDrewU/">Facebook <Icon as={AiFillFacebook} /></NavLink>
         <NavLink href="https://www.instagram.com/tedxdrewuniversity/">Instagram <Icon as={AiFillInstagram} /></NavLink>
       </VStack>
+      <VStack w={'50%'}>
+        <Text pl="8%" fontSize="lg">
+          <Text fontSize="3xl" color={tedxRed}>
+            What is TEDx?</Text>
+          <br />
+          In the spirit of ideas worth spreading, TED has created a program called TEDx. TEDx is a program
+          of local, self-organized events that bring people together to share a TED-like experience. Our event is
+          called TEDxDrewUniversity, where x = independently organized TED event. At our TEDxDrewUniversity event,
+          TEDTalks video
+          and live speakers will combine to spark deep discussion and connection in a small group. The TED Conference
+          provides general guidance for the TEDx program, but individual TEDx events, including ours, are
+          self-organized.
+        </Text>
+      </VStack>
     </Stack>
     <Divider />
     <Text pt="2%" px="4%" textAlign="center" pb="2%">
       This independent TEDx event is operated under license from TED. Learn more at{' '}
-      <Link color={tedxRed} href="ted.com/tedx">ted.com/tedx</Link>
+      <Link color={tedxRed} href="https://ted.com/tedx">ted.com/tedx</Link>
     </Text>
     <Link href="/login">
       <IconButton colorScheme="red"
